2e3fd8f0fb2a1c06cd6a24651e42345f28d038cc
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2023-02-16 22:30:53 -0500

    Add files via upload

---
+++ terraform-eks/variables.tf
@@ -0,0 +1,14 @@
+#
+# Variables Configuration
+#
+
+variable "cluster-name" {
+  default = "tsp-cluster"
+  type    = string
+}
+variable "key_pair_name" {
+  default = "tspcloud"
+}
+variable "eks_node_instance_type" {
+  default = "t2.micro"
+}
